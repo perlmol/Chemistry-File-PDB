f916324fa9655fef3d20d2924e05a32483eb78a7
Ivan Tubert-Brohman <ivan.tubert@gmail.com>
2003-09-26 21:15:13 +0000

    Made it work with MacroMol, tested it with Theresa's files.

--- PDB.pm
+++ PDB.pm
@@ -1,6 +1,9 @@
 package Chemistry::File::PDB;
 
-use Chemistry::Mol;
+$VERSION = '0.05';
+
+use Chemistry::MacroMol;
+use Chemistry::Domain;
 use Carp;
 use strict;
 use vars qw(@ISA @EXPORT @EXPORT_OK %EXPORT_TAGS $VERSION);
@@ -17,12 +20,10 @@ Chemistry::File::PDB
 
 =cut
 
-$VERSION = '0.1';
 
 require Exporter;
 
 @ISA = qw(Exporter);
-
 
 @EXPORT = qw(  );
 
@@ -45,44 +46,55 @@ so that PDB files may be identified and read by Chemistry::Mol::read_mol().
 
 =item pdb_read($fname)
 
-Returns a list of Mol objects from the specified PDB file.
+Returns a Chemistry::MacroMol object from the specified PDB file.
 
 =cut
 
 Chemistry::Mol::register_type("pdb",  read => \&pdb_read,
     is => \&is_pdb, );
 
-sub pdb_read($) {
+sub pdb_read {
     my $fname = shift;
-    my $mol; # a molecule
+    my ($mol) = @_; # a molecule
     my @mols; 
-    my $n_mol;
-    my $n_atom;
-    my ($symbol, $name, $x, $y, $z);
+    my ($n_mol, $n_atom);
+    my $n_res = 0;
+    my $domain;
 
     open F, $fname or croak "Could not open file $fname";
 
-    $mol = new Chemistry::Mol(id => "mol". ++$n_mol);
+    $mol ||= Chemistry::MacroMol->new(id => "mol". ++$n_mol);
     while (<F>) {
 	if (/^TER/) {
-	    $mol->{name} = $name;
-	    push @mols, $mol;
-	    $mol = new Chemistry::Mol(id => "mol". ++$n_mol);
-	    $n_atom = 0;
+#	    $mol->{name} = $name;
+	    #push @mols, $mol;
+	    #$mol = new Chemistry::Mol(id => "mol". ++$n_mol);
+	    #$n_atom = 0;
 	} elsif (/^ATOM/) {
-	    ($symbol, $name, $x, $y, $z) = 
-		unpack "x12A2x3A3x10A8A8A8", $_;
+	    my ($symbol, $suff, $res_name, $seq_n, $x, $y, $z) = 
+		unpack "x12A2A2x1A3x2A4x4A8A8A8", $_;
 	    #print "S:$symbol; N:$name; x:$x; y:$y; z:$z\n";
-	    $mol->add_atom(new Chemistry::Atom(
+            $seq_n =~ s/ //g;
+            if ($seq_n != $n_res) {
+                $domain = Chemistry::Domain->new(parent=>$mol, name=>$res_name,
+                    type => $res_name, id => "d".$seq_n);
+                $n_res = $seq_n;
+                $mol->add_domain($domain);
+            }
+            my $atom_name = $symbol.$suff;
+            $atom_name =~ s/ //g;
+	    $domain->new_atom(
 		symbol => $symbol, 
 		coords => [$x, $y, $z], 
-		id    => "$mol->{id}-$name-a".++$n_atom)
+		#id    => "$mol->{id}-$res_name-a".++$n_atom,
+		id    => "a".++$n_atom,
+                name => $atom_name,
 	    );
 	}
     }
     close F;
 
-    return @mols;
+    return $mol;
 }
 
 =item is_pdb($fname)
@@ -113,15 +125,11 @@ sub is_pdb {
 
 =head1 SEE ALSO
 
-Chemistry::Mol
+Chemistry::MacroMol
 
 =head1 AUTHOR
 
-Ivan Tubert-Brohman <ivan@tubert.org>
-
-=head1 VERSION
-
-$Id$
+Ivan Tubert-Brohman <itub@cpan.org>
 
 =cut
 
